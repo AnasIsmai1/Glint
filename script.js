d1147cb17426fecf1a064a55c5f15f82481fa61c
AnasIsmai1 <anas.ismail1911@gmail.com>
2024-07-22 01:57:12 +0500

    added animations and counter animation to stats

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ const emailError = document.querySelector('.email-error');
 const button = document.getElementById('email-2-submit');
 const formOne = document.getElementById('form-1');
 const formTwo = document.getElementById('form-2');
-let emailValid = false;
+const stats = document.querySelectorAll('.stats');
+const stat = document.querySelectorAll('.stat');
+const heroWrapper = document.querySelector('.hero-wrapper');
+const secOneSpan = document.querySelector('.sec-1-span');
+const secOneHeading = document.querySelector('.sec-1-h2');
+const secOnePara = document.querySelector('.sec-1-p');
+const secTwoSpan = document.querySelector('.sec-2-span');
+const secTwoHeading = document.querySelector('.sec-2-h2');
+const boxes = document.querySelectorAll('.box');
+const secFourSpan = document.querySelector('.sec-4-span');
+const secFourHeading = document.querySelector('.sec-4-h2');
+const scrollBar = document.querySelector('.scroll-bar');
+const secFiveSpan = document.querySelector('.sec-5-span');
+const secFiveHeading = document.querySelector('.sec-5-h2'); 
 
-const regex = /^(?=[^@]+@)([a-zA-Z0-9-]+)@([a-zA-Z0-9-]+)\.([a-zA-Z]{2,6})(\.[a-zA-Z]{1,2})?$/
+let emailValid = false;
+let statShow = false;
+
+const regex = /^(?=[^@]+@)([a-zA-Z0-9-\._]+)@([a-zA-Z0-9-]+)\.([a-zA-Z]{2,6})(\.[a-zA-Z]{1,2})?$/
 
 menu.addEventListener('click', function(){
     console.log("open menu!");  
@@ -64,3 +80,82 @@ formTwo.addEventListener('submit', function(e){
         emailError.classList.add('show');
     }
 })
+
+
+const observer = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+        if(entry.isIntersecting) observer.unobserve(entry.target);
+    })
+}, 
+{
+    threshold : .75,
+}
+)
+
+const conObserver = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+        if(entry.isIntersecting) {
+            observer.unobserve(entry.target)
+        };
+    })
+}, 
+{
+    threshold : .35,
+}
+)
+
+
+const statObserver = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+        if(entry.isIntersecting) {
+            statObserver.unobserve(entry.target);
+                stat.forEach(st => {
+                    let initValue =+ 0;
+                    let hId = st.getAttribute('data-targetH');
+                    let heading = document.querySelector(`.${hId}`);
+                    let target = heading.getAttribute('data-target');
+                    let speed = 10;
+                    let newSpeed = (target / speed) / 10;
+    
+                    let counter = setInterval(function(){
+                        initValue += newSpeed;
+                        console.log(initValue);
+                        heading.textContent = Math.floor(initValue);
+                        if(initValue >= target-1) {
+                            clearInterval(counter);
+                        }
+                    }, 10)
+                })
+        }
+    })
+}, 
+{
+    threshold : .65,
+}
+)
+
+stats.forEach(
+    st => {
+        statObserver.observe(st);
+    } 
+)
+
+boxes.forEach( box => {
+    observer.observe(box);
+})
+
+observer.observe(heroWrapper);
+observer.observe(secOneSpan);
+observer.observe(secOneHeading);
+observer.observe(secOnePara);
+observer.observe(secTwoSpan);
+observer.observe(secTwoHeading);
+observer.observe(secFourSpan);
+observer.observe(secFourHeading);
+observer.observe(scrollBar);
+observer.observe(secFiveSpan);
+observer.observe(secFiveHeading);
+observer.observe(formOne);
